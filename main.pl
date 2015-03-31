a3ab16f2ea03f51c3da2aa500259cacf8f1e40b9
blurryroots <blurryroots@posteo.de>
2015-03-31 16:05:08 +0200

    init of lab 01
    added assignment document and added prototype code

--- main.pl
+++ main.pl
@@ -1,5 +1,26 @@
 
+person('Lara Croft').
+person('Lucy Lu').
+person('Sina Rose').
+
+person('Hans Gruber').
+person('Karl Heinz').
+person('Kurt Bach').
+
+father('Hans Gruber', 'Karl Heinz').
+mother('Lara Croft', 'Karl Heinz').
+
+offspring(Offspring, Parent) :-
+	father(Parent, Offspring);
+	mother(Parent, Offspring).
+
+parents(Offspring, Parents) :-
+	findall(Parent, offspring(Offspring, Parent), Parents).
+
 main :-
 	write('Hey there!'),
-	nl
+	nl,
+	(parents('Karl Heinz', Parents),
+	 write(Parents),
+	 nl)
 	.
