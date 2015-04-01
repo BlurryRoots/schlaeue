4de4affa0b43af3f9fb4718164cbbb0e4a9d686d
blurryroots <blurryroots@posteo.de>
2015-04-01 16:22:39 +0200

    added grandparents and removed test output

--- src/main.pl
+++ src/main.pl
@@ -6,9 +6,12 @@ person('Sina Rose').
 person('Hans Gruber').
 person('Karl Heinz').
 person('Kurt Bach').
+person('Otto Normalverbraucher').
 
 father('Hans Gruber', 'Karl Heinz').
 mother('Lara Croft', 'Karl Heinz').
+
+father('Otto Normalverbraucher', 'Lara Croft').
 
 offspring(Offspring, Parent) :-
 	father(Parent, Offspring);
@@ -17,15 +20,26 @@ offspring(Offspring, Parent) :-
 parents(Offspring, Parents) :-
 	findall(Parent, offspring(Offspring, Parent), Parents).
 
+grandparents(Offspring, Grandparents) :-
+	findall(
+		Grandparent,
+		(offspring(Offspring, Parent),
+		 offspring(Parent, Grandparent)),
+		Grandparents
+	).
+
 write_list([]).
 write_list([Head | Tail]) :-
 	write(Head), nl,
 	write_list(Tail).
 
 main :-
-	write('Hey there!'),
-	nl,
-	(parents('Karl Heinz', Parents),
-	 write_list(Parents),
-	 nl)
+	(parents('Karl Heinz', Parents)
+	,write('Parents of Karl Heinz: ')
+	,write(Parents)
+	,nl),
+	(grandparents('Karl Heinz', Grandparents)
+	,write('Grandparents of Karl Heinz: ')
+	,write(Grandparents)
+	,nl)
 	.
