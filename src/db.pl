7b954817a9dae5df36deebb67cfc61c14bf74758
blurryroots <blurryroots@posteo.de>
2015-04-11 16:52:32 +0200

    changed db layout
    using ids to do lookups

--- src/db.pl
+++ src/db.pl
@@ -2,45 +2,65 @@
 :- discontiguous married/2.
 :- discontiguous parent/2.
 
-female('Lara Croft').
-female('Lucy Lu').
-female('Sina Rose').
-female('Karla Kolumna').
-female('Alesya Kracivaya').
-female('Karoline Koks').
-female('Karina Käse').
+person(1, 'Karina Käse').
+person(2, 'Sina Käse').
+person(3, 'Karolina Koks').
+person(4, 'Karla Kolumna').
+person(5, 'Lara Normalverbraucher').
+person(6, 'Alesya Kolumna').
+person(7, 'Lucy Koks').
+person(8, 'Markus Käse').
+person(9, 'Otto Normalverbraucher').
+person(10, 'Tom Käse').
+person(11, 'Frank Koks').
+person(12, 'Kurt Normalverbraucher').
+person(13, 'Hans Koks').
+person(14, 'Max Kolumna').
+person(15, 'Karl Koks').
 
-male('Hans Gruber').
-male('Karl Heinz').
-male('Kurt Bach').
-male('Otto Normalverbraucher').
-male('Max Mustermann').
-male('Frank Frei').
-male('Markus Malz').
-male('Tom Tulpe').
+female(1).
+female(2).
+female(3).
+female(4).
+female(5).
+female(6).
+female(7).
 
-married('Karina Käse', 'Markus Malz').
-parent('Karina Käse', 'Tom Tulpe').
-parent('Makus Malz', 'Tom Tulpe').
+male(8).
+male(9).
+male(10).
+male(11).
+male(12).
+male(13).
+male(14).
+male(15).
 
-married('Sina Rose', 'Otto Normalverbraucher').
-parent('Sina Rose', 'Lara Croft').
-parent('Otto Normalverbraucher', 'Lara Croft').
-parent('Sina Rose', 'Kurt Bach').
-parent('Otto Normalverbraucher', 'Kurt Bach').
+married(1, 8).
+married(2, 9).
+married(3, 11).
+married(4, 12).
+married(5, 13).
 
-married('Karolina Koks', 'Frank Frei').
-parent('Karolina Koks', 'Hans Gruber').
-parent('Frank Frei', 'Hans Gruber').
-
-married('Lara Croft', 'Hans Gruber').
-parent('Lara Croft', 'Karl Heinz').
-parent('Hans Gruber', 'Karl Heinz').
-parent('Lara Croft', 'Lucy Lu').
-parent('Hans Gruber', 'Lucy Lu').
-
-married('Karla Kolumna', 'Kurt Bach').
-parent('Karla Kolumna', 'Alesya Kracivaya').
-parent('Kurt Bach', 'Alesya Kracivaya').
-parent('Karla Kolumna', 'Max Mustermann').
-parent('Kurt Bach', 'Max Mustermann').
+% Karina & Markus
+parent(1, 2).
+parent(8, 2).
+parent(1, 10).
+parent(8, 10).
+% Sina & Tom
+parent(2, 5).
+parent(9, 5).
+parent(2, 12).
+parent(9, 12).
+% Karolina & Frank
+parent(3, 13).
+parent(11, 13).
+% Karla & Kurt
+parent(4, 6).
+parent(12, 6).
+parent(4, 14).
+parent(12, 14).
+% Lara & Hans
+parent(5, 7).
+parent(13, 7).
+parent(5, 15).
+parent(13, 15).
--- src/pedigree.pl
+++ src/pedigree.pl
@@ -1,28 +1,45 @@
 
+% father and mother
+mother(Parent, Child) :-
+	person(ParentID, Parent),
+	person(ChildID, Child),
+	female(ParentID), parent(ParentID, ChildID).
+father(Parent, Child) :-
+	person(ParentID, Parent),
+	person(ChildID, Child),
+	male(ParentID), parent(ParentID, ChildID).
+
 % is Offspring a child of Parent
 child(Child, Parent) :-
 	father(Parent, Child).
 child(Child, Parent) :-
 	mother(Parent, Child).
 
-mother(Parent, Child) :-
-	female(Parent), parent(Parent, Child).
-father(Parent, Child) :-
-	male(Parent), parent(Parent, Child).
-
 parents(Parents, Child) :-
 	findall(Parent, child(Child, Parent), Parents).
 
+children(Children, Parent) :-
+	findall(Child, child(Child, Parent), Children).
+
 % is Offspring a grandchild of Parent
 grand_child(Grandchild, Grandparent) :-
-	parent(Parent, Grandchild),
-	parent(Grandparent, Parent).
+	person(GrandchildID, Grandchild),
+	person(GrandparentID, Grandparent),
+	parent(ParentID, GrandchildID),
+	parent(GrandparentID, ParentID).
 
 grand_parents(Grandparents, Grandchild) :-
 	findall(
 		Grandparent,
 		grand_child(Grandchild, Grandparent),
 		Grandparents
+	).
+
+grand_children(Grandchildren, Grandparent) :-
+	findall(
+		Grandchild,
+		grand_child(Grandchild, Grandparent),
+		Grandchildren
 	).
 
 descendent(Person, Ancestor) :-
@@ -48,9 +65,11 @@ sister(Person, AnotherPerson) :-
 	female(Person), sibling(Person, AnotherPerson).
 
 parent_sibling(Person, AnotherPerson) :-
-	parent(Parent, Person),
-	parent(AnotherParent, AnotherPerson),
-	sibling(Parent, AnotherParent).
+	person(PersonID, Person),
+	person(AnotherPersonID, AnotherPerson),
+	parent(ParentID, PersonID),
+	parent(AnotherParentID, AnotherPersonID),
+	sibling(ParentID, AnotherParentID).
 
 niece(Person, AnotherPerson) :-
 	female(Person),
