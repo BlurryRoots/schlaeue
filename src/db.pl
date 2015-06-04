6a6a1a4b75231795615fbe2ef3dbc6f9319e3777
blurryroots <blurryroots@posteo.de>
2015-06-04 15:38:34 +0200

    added basic knowledge base

---
+++ src/db.pl
@@ -0,0 +1,30 @@
+% colored houses
+house(red).
+house(green).
+house(ivory).
+house(blue).
+house(yellow).
+% poeple living in said houses
+inhabitant(englishman).
+inhabitant(spaniard).
+inhabitant(ukrainian).
+inhabitant(norwegian).
+inhabitant(japanese).
+% the pet of said inhabitants
+pet(dog).
+pet(snake).
+pet(zebra).
+pet(fox).
+pet(horse).
+% the drinks those inhabtiants drink
+drink(tea).
+drink(orange_juice).
+drink(milk).
+drink(water).
+drink(coffee).
+% the cigarettes they smoke
+cigartte(old_gold).
+cigartte(kools).
+cigartte(chesters).
+cigartte(lucky_strike).
+cigartte(parliament).
