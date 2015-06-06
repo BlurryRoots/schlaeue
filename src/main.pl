3004cd15bd7534a7b89bb75c14166f731aedd57f
blurryroots <blurryroots@posteo.de>
2015-06-06 19:03:51 +0200

    printing the community

--- src/main.pl
+++ src/main.pl
@@ -1,5 +1,13 @@
 
+print_community([], _Number).
+print_community([Flat | Community], Number) :-
+	write('In flat nr('), write(Number), write('): '), write(Flat), nl,
+	NextNumber is Number + 1,
+	print_community(Community, NextNumber).
+print_community(Community) :-
+	print_community(Community, 1).
+
 main :-
-	write('Hey there!'),
-	nl
-	.
+	consult('src/einstein'),
+	solve(Community),
+	print_community(Community).
