55267c9c532dddd364a684fcb4b7350b0fbedc04
blurryroots <blurryroots@posteo.de>
2015-06-06 18:56:14 +0200

    there we go, they really shouldnt :smoking:

--- src/einstein.pl
+++ src/einstein.pl
@@ -1,31 +1,61 @@
+% module for solving a einstein puzzle
 
-/*
-	(red, green, ivory, blue, yellow)
-	(englishman, spaniard, ukrainian, norwegian, japanese)
-	(dog, snake, zebra, fox, horse)
-	(tea, orange_juice, milk, water, coffee)
-	(old_gold, kools, chesterfield, lucky_strike, parliament)
+/* problem domains
+	color (red, green, ivory, blue, yellow)
+	inhabitant (englishman, spaniard, ukrainian, norwegian, japanese)
+	pet (dog, snake, zebra, fox, horse)
+	drink (tea, orange_juice, milk, water, coffee)
+	cigarette (old_gold, kools, chesterfield, lucky_strike, parliament)
+
+	Entry [Color, Inhabitant, Pet, Drink, Cigarette]
 */
 
-% Entry [Color, Inhabitant, Pet, Drink, Cigarette]
+% checks if the resident has the first house in the community
+first(A, [A | _Rest]).
+
+% checks if A lives (directly) to the right of B
+right_of(A, B, [B, A | _Rest]).
+right_of(A, B, [_ | Rest]) :-
+	right_of(A, B, Rest).
+
+% checks if A lives either to the right or the left of B
+next_to(A, B, Community) :-
+	right_of(A, B, Community).
+next_to(A, B, Community) :-
+	right_of(B, A, Community).
 
 solve(Community) :-
 % 1) There are five houses in a row , each of a different color
 	length(Community, 5),
 % 2) The Englishman lives in the red house.
-	member([], Community),
+	member([red, englishman, _, _, _], Community),
 % 3) The Spaniard owns the dog.
+	member([_, spaniard, dog, _, _], Community),
 % 4) Coffee is drunk in the green house.
+	member([green, _, _, coffee, _], Community),
 % 5) The Ukrainian drinks tea.
+	member([_, ukrainian, _, tea, _], Community),
 % 6) The green house is immediately to the right of the ivory house.
+	right_of([green, _, _, _, _], [ivory, _, _, _, _], Community),
 % 7) The Old Gold smoker owns the snake.
+	member([_, _, snake, _, old_gold], Community),
 % 8) Kools are smoked in the yellow house.
+	member([yellow, _, _, _, kools], Community),
 % 9) Milk is drunk in the middle house.
+	[_, _, [_, _, _, milk, _], _, _] = Community,
 %10) The Norwegian lives in the first house on the left.
+	[[_, norwegian, _, _, _], _, _, _, _] = Community,
 %11) The man who smokes Chesterfields lives in the house next to the man with the fox.
+	next_to([_, _, _, _, chesterfield], [_, _, fox, _, _], Community),
 %12) Kools are smoked in the house next to the house where the horse is kept.
+	next_to([_, _, _, _, kools], [_, _, horse, _, _], Community),
 %13) The Lucky Strike smoker drinks orange juice.
+	member([_, _, _, orange_juice, lucky_strike], Community),
 %14) The Japanese smoke Parliaments.
+	member([_, japanese, _, _, parliament], Community),
 %15) The Norwegian lives next to the blue house
-
-	true.
+	next_to([_, norwegian, _, _, _], [blue, _, _, _, _], Community),
+% Missing membership of a water drinker
+	member([_, _, _, water, _], Community),
+% Missing membership of the person owning the zebra
+	member([_, _, zebra, _, _], Community).
--- src/einstein.plt
+++ src/einstein.plt
@@ -2,10 +2,41 @@
 
 :- begin_tests(einstein).
 
+/* problem domains
+	color (red, green, ivory, blue, yellow)
+	inhabitant (englishman, spaniard, ukrainian, norwegian, japanese)
+	pet (dog, snake, zebra, fox, horse)
+	drink (tea, orange_juice, milk, water, coffee)
+	cigarette (old_gold, kools, chesterfield, lucky_strike, parliament)
+
+	Entry [Color, Inhabitant, Pet, Drink, Cigarette]
+*/
+
 prepare :-
 	consult('src/einstein').
 
-test(solving, [setup(prepare), fail]) :-
-	solve(_).
+test(right_side_relation, [setup(prepare)]) :-
+	Community = [[hans], [karl], [julia], [laura]],
+	assertion(first([hans], Community)).
+
+test(right_side_relation, [setup(prepare)]) :-
+	Community = [[hans], [karl], [julia], [laura]],
+	assertion(right_of([karl], [hans], Community)).
+
+test(next_relation, [setup(prepare)]) :-
+	Community = [[hans], [karl], [julia], [laura]],
+	assertion(next_to([laura], [julia], Community)).
+
+% testing the result for correctness
+% see https://de.wikipedia.org/wiki/Zebrar%C3%A4tsel for possible solutions
+% in this exercise the relations are right (instead of left)
+test(solving, [setup(prepare), nondet]) :-
+	solve(Community),
+	assertion(length(Community, 5)),
+	assertion(member([ivory, spaniard, dog, orange_juice, lucky_strike], Community)),
+	assertion(member([green, japanese, zebra, coffee, parliament], Community)),
+	assertion(member([red, englishman, snake, milk, old_gold], Community)),
+	assertion(member([blue, ukrainian, horse, tea, chesterfield], Community)),
+	assertion(member([yellow, norwegian, fox, water, kools], Community)).
 
 :- end_tests(einstein).
