5d8215312cbf4b2c96956927e5bdd6c7d447d0d8
blurryroots <blurryroots@posteo.de>
2015-06-06 15:24:01 +0200

    reformulated constraints for generate and test approach

--- src/einstein.pl
+++ src/einstein.pl
@@ -31,111 +31,61 @@ cigarette(chesters).
 cigarette(lucky_strike).
 cigarette(parliament).
 
+next_to(Nr, Nr2) :-
+	D is Nr - Nr2, abs(D, 1).
+right_of(Nr, Nr2) :-
+	Nr is Nr2 - 1.
+
 % House list signature
-% 	[Color, Inhabitant, Pet, Drink, Cigarette]
-
-%  1) Five houses in a row, all have different colors.
-%  2) Englishman in red house.
-check_englishman([]) :-
-	false.
-check_englishman([House | Rest]) :-
-	[red, englishman | _] = House;
-	check_englishman(Rest).
-%  3) Spaniard owns dog.
-check_spaniard([]) :-
-	false.
-check_spaniard([House | Rest]) :-
-	[_, spaniard, dog | _] = House;
-	check_spaniard(Rest).
-%  4) Coffee in green house.
-check_coffee([]) :-
-	false.
-check_coffee([House | Rest]) :-
-	[green, _, _, coffee, _] = House;
-	check_coffee(Rest).
-%  5) Ukrainian drinks tea.
-check_ukrainian([]) :-
-	false.
-check_ukrainian([House | Rest]) :-
-	[_, ukrainian, _, tea, _] = House;
-	check_ukrainian(Rest).
-%  6) Green house right of ivory house.
-check_greenhouse([]) :-
-	false.
-check_greenhouse([House, RightHouse | Rest]) :-
-	(
-		[green | _] = House,
-		[ivory | _] = RightHouse
-	);
-	check_greenhouse(Rest).
-%  7) Old Gold smoker owns snake.
-check_oldgold([]) :-
-	false.
-check_oldgold([House | Rest]) :-
-	[_, _, snake, _, old_gold] = House;
-	check_oldgold(Rest).
-%  8) Kools smoker in yellow house.
-% 12) Kools smoker in house next to house with horse
-check_kools_helper([yellow, _, _, _, kools], [_, _, horse, _, _]).
-check_kools_helper([_, _, horse, _, _], [yellow, _, _, _, kools]).
-check_kools([]) :-
-	false.
-check_kools([House, RightHouse | Rest]) :-
-	check_kools_helper(House, RightHouse);
-	check_kools(Rest).
-%  9) Milk in middle house.
-check_kools([_, _, House, _, _]) :-
-	[_, _, _, milk, _] = House.
-% 10) Norwegian first house left.
-% 15) Norwegian lives next to blue house
-check_norwegian([House, RightHouse, _, _, _]) :-
-	[_, norwegian, _, _, _] = House,
-	[blue, _, _, _, _] = RightHouse.
-% 11) Chesterfields smoker in house next to man with fox.
-check_chesterfields_helper([_, _, _, _, chesters], [_, _, fox, _, _]).
-check_chesterfields_helper([_, _, fox, _, _], [_, _, _, _, chesters]).
-check_chesterfields([]) :-
-	false.
-check_chesterfields([House, RightHouse | Rest]) :-
-	check_chesterfields_helper(House, RightHouse);
-	check_chesterfields(Rest).
-% 13) Lucky Strike smoker drinks orange juice
-check_lucky([]) :-
-	false.
-check_lucky([[_, _, _, orange_juice, lucky_strike] | _]).
-check_lucky([_ | Rest]) :-
-	check_lucky(Rest).
-% 14) Japanese smokes Parliament
-check_japanese([]) :-
-	false.
-check_japanese([[_, japanese, _, _, parliament] | _]).
-check_japanese([_ | Rest]) :-
-	check_lucky(Rest).
-
+% 	[Nr, Color, Inhabitant, Pet, Drink, Cigarette]
 generate([A, B, C, D, E]) :-
 	findall(X, color(X), Colors), permutation(Colors, [CoA, CoB, CoC, CoD, CoE]),
 	findall(X, inhabitant(X), Inhabitants), permutation(Inhabitants, [InA, InB, InC, InD, InE]),
 	findall(X, pet(X), Pets), permutation(Pets, [PeA, PeB, PeC, PeD, PeE]),
 	findall(X, drink(X), Drinks), permutation(Drinks, [DrA, DrB, DrC, DrD, DrE]),
 	findall(X, cigarette(X), Cigarettes), permutation(Cigarettes, [CiA, CiB, CiC, CiD, CiE]),
-	A = [CoA, InA, PeA, DrA, CiA],
-	B = [CoB, InB, PeB, DrB, CiB],
-	C = [CoC, InC, PeC, DrC, CiC],
-	D = [CoD, InD, PeD, DrD, CiD],
-	E = [CoE, InE, PeE, DrE, CiE].
+	A = [1, CoA, InA, PeA, DrA, CiA],
+	B = [2, CoB, InB, PeB, DrB, CiB],
+	C = [3, CoC, InC, PeC, DrC, CiC],
+	D = [4, CoD, InD, PeD, DrD, CiD],
+	E = [5, CoE, InE, PeE, DrE, CiE].
 
 test(Houses) :-
-	check_englishman(Houses),
-	check_spaniard(Houses),
-	check_coffee(Houses),
-	check_ukrainian(Houses),
-	check_greenhouse(Houses),
-	check_oldgold(House),
-	check_kools(House),
-	check_norwegian(House),
-	check_chesterfields(House),
-	check_lucky(House),
-	check_japanese(House).
+%  1) Five houses in a row, all have different colors.
+%  2) Englishman in red house.
+	member([_, red, englishman | _], Houses),
+%  3) Spaniard owns dog.
+	member([_, _, spaniard, dog | _], Houses),
+%  4) Coffee in green house.
+%  6) Green house right of ivory house.
+	member([NrGreen, green, _, _, coffee, _], Houses),
+	member([NrIvory, ivory | _], Houses),
+	right_of(NrGreen, NrIvory),
+%  5) Ukrainian drinks tea.
+	member([_, _, _, ukrainian, _, tea, _], Houses),
+%  7) Old Gold smoker owns snake.
+	member([_, _, _, snake, _, old_gold], Houses),
+%  8) Kools smoker in yellow house.
+% 12) Kools smoker in house next to house with horse
+	member(House, [NrYellow, yellow, _, _, _, kools], Houses),
+	member(House, [NrHorse_, _, horse, _, _], Houses),
+	next_to(NrYellow, NrHorse_),
+%  9) Milk in middle house.
+	member([3, _, _, _, milk, _], Houses),
+% 10) Norwegian first house left.
+% 15) Norwegian lives next to blue house
+	member([NrNorwegian, _, norwegian, _, _, _], Houses),
+	NrNorwegian = 1,
+	member([NrBlue, blue, _, _, _, _], Houses),
+	next_to(NrNorwegian, NrBlue),
+% 11) Chesterfields smoker in house next to man with fox.
+	member([NrFox, _, _, fox, _, _], Houses),
+	member([NrChester, _, _, _, _, chesters], Houses),
+	next_to(NrFox, NrChester),
+% 13) Lucky Strike smoker drinks orange juice
+	member([_, _, _, _, orange_juice, lucky_strike], Houses),
+% 14) Japanese smokes Parliament
+	member([_, _, japanese, _, _, parliament], Houses).
 
 solve(Result) :-
 	generate(Result),
