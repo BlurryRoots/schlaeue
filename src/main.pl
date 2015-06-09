5334c0f406c21a7f0b9fe2f80b7774a03948645e
blurryroots <blurryroots@posteo.de>
2015-06-09 14:01:29 +0200

    updated main

--- src/main.pl
+++ src/main.pl
@@ -1,4 +1,4 @@
-
+% Prints each flat with its attributes (Color, Inhabitant, Pet, Drink, Smoke)
 print_community([], _Number).
 print_community([Flat | Community], Number) :-
 	write('In flat nr('), write(Number), write('): '), write(Flat), nl,
@@ -7,11 +7,20 @@ print_community([Flat | Community], Number) :-
 print_community(Community) :-
 	print_community(Community, 1).
 
+% Runs the test and generate approach.
 run_testgenerate :-
+	write('Running test generate approach:'), nl,
 	consult('src/einstein_testgenerate'),
-	solve(Community),
+	time(solve(Community)),
+	print_community(Community).
+
+% Runs the constraints approach.
+run_constrainst :-
+	write('Running constraints approach:'), nl,
+	consult('src/einstein_constraints'),
+	time(solve(Community)),
 	print_community(Community).
 
 main :-
-	write('Running test generate approach:'), nl,
-	run_testgenerate.
+	run_testgenerate, nl,
+	run_constrainst, nl.
