94b41abf8f5b8795a799d890204cbafbce14cd70
blurryroots <blurryroots@posteo.de>
2015-06-18 12:38:39 +0200

    move check now in its own function

--- src/toadsnfrogs.py
+++ src/toadsnfrogs.py
@@ -1,11 +1,18 @@
+EMPTY = '#'
+TOAD = 't'
+FROG = 'f'
+
 def is_toad (board, pos):
-    return TOAD == board[pos]
+    name = board[pos]
+    return TOAD == name[:1]
 
 def is_frog (board, pos):
-    return FROG == board[pos]
+    name = board[pos]
+    return FROG == name[:1]
 
 def is_empty (board, pos):
-    return EMPTY == board[pos]
+    name = board[pos]
+    return EMPTY == name[:1]
 
 def is_in_bounds (board, pos):
     return len (board) > pos
@@ -14,28 +21,32 @@ def swap (board, pos, otherpos):
     board[pos], board[otherpos] = board[otherpos], board[pos]
     return board
 
-def move (board, animal, nr):
-    name = animal + str (nr)
-    pos = list.index (board, name)
-
-    if not is_in_bounds (board, pos + 1):
-        return False
-
+def can_move (board, animal, pos):
+    sign = 0
     if TOAD == animal:
-        if is_empty (board, pos + 1):
-            return True, swap (board, pos, pos + 1)
-        if is_frog (board, pos + 1) \
-        and is_in_bounds (board, pos + 2) and is_empty (board, pos + 2):
-            return True, swap (board, pos, pos + 2)
-        else:
-            return False, board
-    if FROG == animal:
-        if is_empty (board, pos - 1):
-            return True, swap (board, pos, pos - 1)
-        if is_toad (board, pos - 1) \
-        and is_in_bounds (board, pos - 2) and is_empty (board, pos - 2):
-            return True, swap (board, pos, pos - 2)
-        else:
-            return False, board
+        sign = 1
+    elif FROG == animal:
+        sign = -1
     else:
         raise Error ("Unkown animal type!")
+
+    step_width = sign * 1
+    if is_empty (board, pos + step_width):
+        return step_width
+
+    step_width = step_width + (sign * 1)
+    if is_frog (board, pos + step_width) \
+    and is_in_bounds (board, pos + step_width) \
+    and is_empty (board, pos + step_width):
+        return step_width
+
+    return 0
+
+
+def move (board, animal_type, nr, step_width):
+    name = animal_type + str (nr)
+    pos = list.index (board, name)
+
+    swap (board, pos, pos + step_width)
+
+    return board
