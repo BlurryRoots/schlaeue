a6d42659c6a911be44e151fd8ab5ee428cc580b1
blurryroots <blurryroots@posteo.de>
2015-06-20 14:16:46 +0200

    more verbose main

--- src/main.py
+++ src/main.py
@@ -2,24 +2,24 @@ from toadsnfrogs import *
 import copy
 
 def game ():
-	board = ['t1', 't2', 't3', '#', '#', 'f1', 'f2', 'f3']
-	number_animals = 3
+	board = ['t1', '#', '#', 'f1']
+	number_animals = 1
 	can_move = True
 	animal_type = FROG
-	boad_copy = copy.deepcopy(board)
 	tree = build_game_tree(
-		boad_copy,
-		TreeNode(animal_type, 1, boad_copy),
+		copy.deepcopy(board),
 		animal_type,
 		number_animals
 	)
 	print("Game Tree:\n" + str(tree))
 	print("Starting game:\n")
-	print(board)
+	print(str(board) + " start")
 	while can_move:
 		can_move, new_board = try_take_turn(board, animal_type, 1, number_animals)
-		print(new_board)
+		print(str(new_board) + " turn by: " + animal_type)
 		animal_type = swap_animal_type(animal_type)
+
+	print(animal_type + " won!")
 
 if __name__ == '__main__':
     game()
