8483c07a26377ad6022fa4d76a27a96a1203b596
Tim <timsn@posteo.de>
2015-06-25 00:16:29 +0200

    klassenbasierter Entwurf

--- src/tests.py
+++ src/tests.py
@@ -1,35 +1,84 @@
 import unittest
-import toadsnfrogs
-
-TOAD = 't'
-FROG = 'f'
-EMPTY = '#'
+from toadsnfrogs_new import Frog, Toad, Space, Board
 
 
 class TestToadsFrogs(unittest.TestCase):
     def setUp(self):
-        self.board = ['t1', 't2', '#', 'f1', 'f2']
+        pass
 
     def test_move(self):
-        new_board = toadsnfrogs.move(self.board, 1, 1)
-        self.assertEqual(new_board, ['t1', '#', 't2', 'f1', 'f2'])
+        t1 = Toad(1)
+        t2 = Toad(2)
+        f1 = Frog(1)
+        f2 = Frog(2)
+        space = Space()
 
-        new_board = toadsnfrogs.move(self.board, 3, -2)
-        self.assertEqual(new_board, ['t1', 'f1', 't2', '#', 'f2'])
+        board = Board([t1, t2, space, f1, f2])
 
-        new_board = toadsnfrogs.move(self.board, 4, -1)
-        self.assertEqual(new_board, ['t1', 'f1', 't2', 'f2', '#'])
+        new_board = board.move(1, 1)
+        self.assertEqual(new_board, Board([t1, space, t2, f1, f2]))
 
-        new_board = toadsnfrogs.move(self.board, 2, 2)
-        self.assertEqual(new_board, ['t1', 'f1', '#', 'f2', 't2'])
+        new_board = new_board.move(3, -2)
+        self.assertEqual(new_board, Board([t1, f1, t2, space, f2]))
 
-    def test_can_move(self):
-        self.assertEqual(1, toadsnfrogs.can_move(self.board, 1))
-        self.assertEqual(0, toadsnfrogs.can_move(self.board, 0))
-        self.assertEqual(-1, toadsnfrogs.can_move(self.board, 3))
-        self.assertEqual(0, toadsnfrogs.can_move(self.board, 4))
+        new_board = new_board.move(4, -1)
+        self.assertEqual(new_board, Board([t1, f1, t2, f2, space]))
+
+        new_board = new_board.move(2, 2)
+        self.assertEqual(new_board, Board([t1, f1, space, f2, t2]))
+
+    def test_cant_move(self):
+        t1 = Toad(1)
+        t2 = Toad(2)
+        f1 = Frog(1)
+        f2 = Frog(2)
+        space = Space()
+
+        board = Board([t1, t2, space, f1, f2])
+        self.assertEqual(0, board.can_move(0))
+        self.assertEqual(0, board.can_move(4))
         with self.assertRaises(TypeError):
-            toadsnfrogs.can_move(self.board, 2)
+            board.can_move(2)
+
+    def test_can_move_one_step(self):
+        t1 = Toad(1)
+        t2 = Toad(2)
+        f1 = Frog(1)
+        f2 = Frog(2)
+        space = Space()
+
+        board = Board([t1, t2, space, f1, f2])
+
+        self.assertEqual(1, board.can_move(1))
+        self.assertEqual(-1, board.can_move(3))
+
+    def test_can_move_two_steps(self):
+        t1 = Toad(1)
+        t2 = Toad(2)
+        f1 = Frog(1)
+        f2 = Frog(2)
+        space = Space()
+
+        board = Board([t1, space, t2, f1, space, f2])
+        self.assertEqual(2, board.can_move(2))
+        self.assertEqual(-2, board.can_move(3))
+
+    def test_possible_moves(self):
+        t1 = Toad(1)
+        t2 = Toad(2)
+        f1 = Frog(1)
+        f2 = Frog(2)
+        space = Space()
+
+        board = Board([t1, t2, space, f1, f2])
+        self.assertEqual(board.get_possible_moves(1), [1])
+        self.assertEqual(board.get_possible_moves(-1), [3])
+
+        board = Board([t1, t2, f1, space, f2])
+        self.assertEqual(board.get_possible_moves(-1), [4])
+
+        board = Board([t1, space, t2, f1, space, f2])
+        self.assertEqual(board.get_possible_moves(1), [0, 2])
 
 if __name__ == '__main__':
     unittest.main()
--- src/toadsnfrogs.py
+++ src/toadsnfrogs.py
@@ -1,159 +1,213 @@
-EMPTY = '#'
-TOAD = 't'
-FROG = 'f'
+from sys import maxsize
 
 
-def is_toad(board, pos):
-    name = board[pos]
-    return TOAD == name[:1]
-
-
-def is_frog(board, pos):
-    name = board[pos]
-    return FROG == name[:1]
-
-
-def is_empty(board, pos):
-    name = board[pos]
-    return EMPTY == name[:1]
-
-
-def is_in_bounds(board, pos):
-    return len(board) > pos
-
-
-def swap(board, pos, otherpos):
-    board[pos], board[otherpos] = board[otherpos], board[pos]
-    return board
-
-
-def can_move(board, pos):
-    if not is_in_bounds(board, pos):
-        return 0
-
-    animal_type = board[pos][:1]
-
-    sign = 0
-    if TOAD == animal_type:
-        sign = 1
-    elif FROG == animal_type:
-        sign = -1
-    else:
-        raise TypeError("Unkown animal type!")
-
-    step_width = sign
-    if (is_in_bounds(board, pos + step_width)
-            and is_empty(board, pos + step_width)):
-        return step_width
-
-    step_width = step_width + sign
-    if (is_in_bounds(board, pos + step_width)
-            and is_frog(board, pos + step_width)
-            and is_empty(board, pos + step_width)):
-        return step_width
-
-    return 0
-
-
-def move(board, pos, step_width):
-    return swap(board, pos, pos + step_width)
-
-
-def take_turn(board, animal_type, nr):
-    name = animal_type + str(nr)
-    pos = list.index(board, name)
-
-    step_width = can_move(board, pos)
-    if 0 != step_width:
-        return True, move(board, pos, step_width)
-    else:
-        return False, board
-
-
-def try_take_turn(board, animal_type, animal_id, number_animals):
-    status, new_board = take_turn(board, animal_type, animal_id)
-    if status:
-        return True, new_board
-    if not status and animal_id < number_animals:
-        return try_take_turn(board, animal_type, animal_id + 1, number_animals)
-    else:
-        return False, new_board
-
-
-def swap_animal_type(animal_type):
-    if FROG == animal_type:
-        return TOAD
-    elif TOAD == animal_type:
-        return FROG
-    else:
-        raise TypeError("NOOOOOO!")
-
-
-class TreeNode():
-    """keks"""
-    def __init__(self, last_animal_type, animal_id, board):
-        self.last_animal_type = last_animal_type
-        self.animal_id = animal_id
-        self.board = board
-        self.alternatives = []
-
-    def add_alternative(self, tree_node):
-        list.append(self.alternatives, tree_node)
+class Frog():
+    """ computer player """
+    def __init__(self, nr):
+        self._nr = nr
+        self.playernum = -1
 
     def __str__(self):
-        alt_str = "[\n"
-        alt_len = len(self.alternatives)
-        alt_idx = 1
-
-        for alt in self.alternatives:
-            alt_str = alt_str + str(alt)
-
-            if alt_idx < alt_len:
-                alt_str = alt_str + ","
-
-            alt_str = alt_str + "\n"
-            alt_idx = alt_idx + 1
-
-        alt_str = alt_str + "]"
-
-        board_len = len(self.board)
-        board_idx = 1
-        board_str = "["
-        for token in self.board:
-            board_str = board_str + "\"" + token + "\""
-
-            if board_idx < board_len:
-                board_str = board_str + ","
-
-            board_idx = board_idx + 1
-
-        board_str = board_str + "]"
-
-        return ("{\n"
-            + "\t\"board\": " + board_str + ",\n"
-            + "\t\"last_animal_type\": \"" + self.last_animal_type + "\",\n"
-            + "\t\"animal_id\": " + str(self.animal_id) + ",\n"
-            + "\t\"alternatives\": " + alt_str
-            + "\n}")
+        return 'F{}'.format(self._nr)
 
 
-import copy
-def build_game_tree (board, node, animal_type, number_animals):
-    board_copy = copy.deepcopy(board)
-    animal_id = 1
-    while number_animals >= animal_id:
-        valid, new_board = take_turn(board_copy, animal_type, animal_id)
-        if valid:
-            new_node = TreeNode(animal_type, animal_id, copy.deepcopy(new_board))
-            # play next turn
-            node.add_alternative(
-                build_game_tree(
-                    new_board,
-                    new_node,
-                    swap_animal_type(animal_type),
-                    number_animals
-                )
-            )
-        animal_id = animal_id + 1
+class Toad():
+    """ human player """
+    def __init__(self, nr):
+        self._nr = nr
+        self.playernum = 1
 
-    return node
+    def __str__(self):
+        return 'T{}'.format(self._nr)
+
+
+class Space():
+    def __init__(self):
+        self.playernum = 0
+
+    def __str__(self):
+        return '__'
+
+    def __repr__(self):
+        str(self)
+
+
+class Board():
+    def __init__(self, board_list):
+        self.board = board_list
+
+    def is_game_over(self, current_player):
+        return self.get_possible_moves(current_player) == []
+
+    def is_in_bounds(self, pos):
+        return pos >= 0 and pos < len(self.board)
+
+    def is_empty(self, pos):
+        return isinstance(self.board[pos], Space)
+
+    def is_toad(self, pos):
+        return isinstance(self.board[pos], Toad)
+
+    def is_frog(self, pos):
+        return isinstance(self.board[pos], Frog)
+
+    def different_animals(self, pos1, pos2):
+        return ((self.is_frog(pos1) and self.is_toad(pos2)) or
+                (self.is_toad(pos1) and self.is_frog(pos2)))
+
+    def get_direction(self, pos):
+        if self.is_toad(pos):
+            return 1
+        elif self.is_frog(pos):
+            return -1
+        else:
+            return 0
+
+    def get_possible_moves(self, playernum):
+        moves = []
+        for i in range(len(self.board)):
+            if self.board[i].playernum == playernum and self.can_move(i):
+                moves.append(i)
+        return moves
+
+    def can_move(self, pos):
+        if not self.is_in_bounds(pos):
+            return 0
+
+        direction = self.get_direction(pos)
+
+        if direction == 0:
+            return 0
+
+        # check one step
+        steps = direction
+        if (self.is_in_bounds(pos + steps)
+                and self.is_empty(pos + steps)):
+            return steps
+
+        # check two steps
+        steps = direction * 2
+        if (self.is_in_bounds(pos + steps)
+                and self.different_animals(pos, pos + direction)
+                and self.is_empty(pos + steps)):
+            return steps
+        return 0
+
+    def move(self, pos, steps):
+        board = list(self.board)
+        board[pos], board[pos + steps] = board[pos + steps], board[pos]
+        return Board(board)
+
+    def __str__(self):
+        helper = '[' + ' ,'.join(
+            ' ' + str(idx) for idx, x in enumerate(self.board)) + ']\n'
+        return helper + '[' + ' ,'.join(str(x) for x in self.board) + ']'
+
+    def __eq__(self, other):
+        if isinstance(other, Board):
+            return other.board == self.board
+        return False
+
+
+def minimax(board, depth, maximizingPlayer):
+    playernum = -1 if maximizingPlayer else 1
+
+    if board.is_game_over(playernum):
+        return 100 * playernum  # calculate real score
+
+    if depth == 0:
+        return 0
+
+    possible_moves = board.get_possible_moves(playernum)
+
+    if maximizingPlayer:
+        bestValue = -maxsize
+        for move in possible_moves:
+            steps = board.can_move(move)
+            board_after_move = board.move(move, steps)
+            val = minimax(board_after_move, depth - 1, False)
+            bestValue = max(bestValue, val)
+        return bestValue
+    else:
+        bestValue = maxsize
+        for move in possible_moves:
+            steps = board.can_move(move)
+            board_after_move = board.move(move, steps)
+            val = minimax(board_after_move, depth - 1, True)
+            bestValue = min(bestValue, val)
+        return bestValue
+
+
+def ask_move(board):
+    pos = -1
+    steps = 0
+    while True:
+        print('which one do you want to move?')
+        raw_choice = input()
+        try:
+            pos = int(float(raw_choice))
+            steps = board.can_move(pos)
+        except:
+            pass
+
+        if steps > 0:
+            break
+        print('incorrect input, try again!')
+    return pos
+
+
+def next_move(board, player):
+    if player == 1:
+        pos = ask_move(board)
+        steps = board.can_move(pos)
+        print('moving {0} by {1} steps'.format(pos, steps))
+
+        return board.move(pos, steps)
+    elif player == -1:
+        print('thinking...')
+        return make_move(board)
+
+
+def make_move(board):
+    moves = board.get_possible_moves(-1)
+    best_move = None
+    best_score = 0
+    for move in moves:
+        score = score_for_move(board, move)
+        if score >= best_score:
+            best_score = score
+            best_move = move
+    print('moving {0} with score of {1}'.format(best_move, score))
+    steps = board.can_move(best_move)
+    return board.move(best_move, steps)
+
+
+def score_for_move(board, move):
+    steps = board.can_move(move)
+    new_board = board.move(move, steps)
+    return minimax(new_board, 15, False)
+
+
+if __name__ == '__main__':
+    t1 = Toad(1)
+    t2 = Toad(2)
+    t3 = Toad(3)
+    f1 = Frog(1)
+    f2 = Frog(2)
+    f3 = Frog(3)
+    space = Space()
+    board = Board([t1, t2, t3, space, space, space, space, f1, f2, f3])
+    player = -1  # computer starts
+
+    print('starting game of frogsntoads')
+    print(board)
+    while not board.is_game_over(player):
+        board = next_move(board, player)
+        player = -player
+        print(board)
+
+    print('game over')
+    if player == 1:
+        print('computer wins')
+    elif player == -1:
+        print('you win!')
